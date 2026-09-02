d4a327c3017590d9bad4f7e786d53d89ad554747
Darryl Gamroth <dgamroth@rubus.ca>
2026-09-01 23:47:05 -0700

    Release PipeWireAO v0.6.1

--- src/PipeWireAO.jl
+++ src/PipeWireAO.jl
@@ -312,7 +312,7 @@ include("stream.jl")
 include("filter.jl")
 include("listeners.jl")
 
-const _MINIMUM_JLL_VERSION = v"1.7.0+8"
+const _MINIMUM_JLL_VERSION = v"1.7.0+9"
 
 function __init__()
     version = pkgversion(LibPipeWire.PipeWireAO_jll)
